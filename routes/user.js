575d54a688a75ce42b887470e46d4c017a720d86
Krishna Kompalli <kompalligk@gmail.com>
2020-06-19 02:54:46 +0530

    done

---
+++ routes/user.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const User = require('../models/user')
+const auth = require('../middleware/auth')
+const router = new express.Router()
+
+
+router.post('/users', async (req, res) => {
+    const user = new User(req.body)
+
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.getAuthToken()
+        res.send({user, token})
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+//remove latest token
+router.post('/users/logout', auth, async (req,res) => {
+    try{
+        req.user.tokens = req.user.tokens.filter((i) => {
+            return i.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+       
+    } catch(e){
+        res.status(500).send()
+    }
+})
+
+
+router.post('/users/logout-all', auth, async (req, res) => {
+    try{
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch(e){
+        res.status(500).send()
+    }   
+})
+
+router.get('/users/current-user', auth, async (req, res) => {
+   res.send(req.user)
+})
+
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('./db/mongoose')
+const userRouter = require('./routes/user')
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json())
+app.use(userRouter)
+
+app.listen(port, ()=>{
+    console.log('port working at '+ port)
+})
+
